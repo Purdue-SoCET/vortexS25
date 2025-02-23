c298d3e862d90c2776a1b8028726e6b3e7f4cd9c
armaankanchan <armaankanchan@hotmail.com>
2025-02-23 14:42:40 -0500

    Update VX_lsu_slice.sv

--- hw/rtl/core/VX_lsu_slice.sv
+++ hw/rtl/core/VX_lsu_slice.sv
@@ -14,8 +14,8 @@
 `include "VX_define.vh"
 
 module VX_lsu_slice import VX_gpu_pkg::*; #(
-    parameter `STRING INSTANCE_ID = ""
-) (
+    parameter `STRING INSTANCE_ID = "",
+       parameter NUM_THREADS = 4) (
     `SCOPE_IO_DECL
 
     input wire              clk,
