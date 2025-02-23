f326874eeb2c084cffe6c2a11ae51f8b7341cd6a
armaankanchan <armaankanchan@hotmail.com>
2025-02-23 13:50:28 -0500

    Update VX_execute.sv

--- hw/rtl/core/VX_execute.sv
+++ hw/rtl/core/VX_execute.sv
@@ -15,9 +15,8 @@
 
 module VX_execute import VX_gpu_pkg::*; #(
     parameter `STRING INSTANCE_ID = "",
-    parameter CORE_ID = 0,
-    parameter NUM_THREADS = 4,
-    parameter NUM_WARPS = 4) (
+    parameter CORE_ID = 0
+    ) (
     `SCOPE_IO_DECL
 
     input wire              clk,
