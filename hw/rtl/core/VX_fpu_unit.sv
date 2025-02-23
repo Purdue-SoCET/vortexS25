e43a893cef147af07ea7f0f0ab982ed5b24c6666
armaankanchan <armaankanchan@hotmail.com>
2025-02-23 13:50:55 -0500

    Update VX_fpu_unit.sv

--- hw/rtl/core/VX_fpu_unit.sv
+++ hw/rtl/core/VX_fpu_unit.sv
@@ -14,8 +14,9 @@
 `include "VX_fpu_define.vh"
 
 module VX_fpu_unit import VX_fpu_pkg::*; #(
-    parameter `STRING INSTANCE_ID = ""
-) (
+    parameter `STRING INSTANCE_ID = "",
+    parameter NUM_THREADS = 4,
+    parameter NUM_WARPS = 4) (
     input wire clk,
     input wire reset,
 
