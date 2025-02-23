12d9bd98c69c12586acd2729bdb8bbc9481cf50a
armaankanchan <armaankanchan@hotmail.com>
2025-02-23 14:45:26 -0500

    Update VX_sfu_unit.sv

--- hw/rtl/core/VX_sfu_unit.sv
+++ hw/rtl/core/VX_sfu_unit.sv
@@ -16,7 +16,8 @@
 module VX_sfu_unit import VX_gpu_pkg::*; #(
     parameter `STRING INSTANCE_ID = "",
     parameter CORE_ID = 0,
-    parameter NUM_THREADS = 4) (
+    parameter NUM_THREADS = 4,
+    parameter NUM_WARPS = 4) (
     input wire              clk,
     input wire              reset,
 
