9b28dd1d4c298ec31817772271addeb76beace82
armaankanchan <armaankanchan@hotmail.com>
2025-02-23 14:49:44 -0500

    Update VX_wctl_unit.sv

--- hw/rtl/core/VX_wctl_unit.sv
+++ hw/rtl/core/VX_wctl_unit.sv
@@ -15,8 +15,9 @@
 
 module VX_wctl_unit import VX_gpu_pkg::*; #(
     parameter `STRING INSTANCE_ID = "",
-    parameter NUM_LANES = 1
-) (
+    parameter NUM_LANES = 1,
+    parameter NUM_THREADS = 4,
+    parameter NUM_WARPS = 4) (
     input wire              clk,
     input wire              reset,
 
