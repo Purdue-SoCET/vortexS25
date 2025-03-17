9ce94ceada7eb3f1770c13a1172004c32c4fae58
Zohaib <ali191@purdue.edu>
2025-03-17 19:01:03 -0400

    found an error

--- hw/rtl/core/VX_core_top.sv
+++ hw/rtl/core/VX_core_top.sv
@@ -145,7 +145,9 @@ module VX_core_top import VX_gpu_pkg::*; #(
 
     VX_core #(
         .INSTANCE_ID (`SFORMATF(("core"))),
-        .CORE_ID (CORE_ID)
+        .CORE_ID (CORE_ID), 
+        .NUM_THREADS(NUM_THREADS),
+        .NUM_WARPS(NUM_WARPS)
     ) core (
         `SCOPE_IO_BIND (0)
         .clk            (clk),
